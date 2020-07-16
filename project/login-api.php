56eae848c3889cd67628fc5a0643f549300e09d4
esther-sing <s209261009@gmail.com>
2020-07-16 16:38:36 +0800

    登入判斷

---
+++ project/login-api.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__. '/0714_connect.php';
+
+$output = [
+    'success' => false,
+    'code' => 0,
+    'error' => '',
+    'postData' => $_POST
+];
+
+if(empty($_POST['email']) or empty($_POST['password'])){
+    $output['error'] = '資料不足';
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$sql = "SELECT * FROM members WHERE email=? AND password=SHA1(?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    $_POST['email'],
+    $_POST['password'],
+]);
+
+$row = $stmt->fetch();
+
+if(! empty($row)){
+    $output['success'] = true;
+    unset($row['password']);
+    $_SESSION['member'] = $row;
+}
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
