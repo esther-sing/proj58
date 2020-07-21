e2051ded862f94ecc05a0bfd0c7ff7aea8d3541e
esther-sing <s209261009@gmail.com>
2020-07-21 11:31:25 +0800

    呈現Array畫面

--- project/cate01.php
+++ project/cate01.php
@@ -17,8 +17,33 @@ foreach($rows as $r){
 <div class="container">
 
     <h2>Hello</h2>
-    <pre><?php // print_r($rows) ?></pre>
+    <pre><?php // print_r($rows) 印出陣列全部 ?></pre>
     <pre><?php print_r($cate) #  印出陣列第一層 ?></pre>
+
+    <!-- 呈現畫面Array
+(
+    [0] => Array
+        (
+            [sid] => 1
+            [name] => 程式設計
+            [parent_sid] => 0
+        )
+
+    [1] => Array
+        (
+            [sid] => 2
+            [name] => 繪圖軟體
+            [parent_sid] => 0
+        )
+
+    [2] => Array
+        (
+            [sid] => 3
+            [name] => 網際網路應用
+            [parent_sid] => 0
+        )
+
+) -->
 
 </div>
 <?php include __DIR__. '/0714_scripts.php' ?>
