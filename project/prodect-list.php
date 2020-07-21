bc2e3260abe9f6f3c5028ffcdcd0e59679ce1f3d
esther-sing <s209261009@gmail.com>
2020-07-21 14:10:38 +0800

    商品列表2

--- project/prodect-list.php
+++ project/prodect-list.php
@@ -23,13 +23,13 @@ $sql = sprintf("SELECT * FROM `products` LIMIT %s, %s", ($page-1)*$perPage, $per
 
 $rows = $pdo->query($sql)->fetchAll();
 
-echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+// echo json_encode($rows, JSON_UNESCAPED_UNICODE);
 
 
 
 
 
-exit;
+// exit;
 
 ?>
 ?>
@@ -38,7 +38,44 @@ exit;
 <?php include __DIR__. '/0714_navebar.php' ?>
 <div class="container">
 
-    <h2>Hello</h2>
+<div class="row">
+    <div class="col-lg-3">
+
+    </div>
+    <div class="col-lg-9">
+        <div class="row">
+            <div class="col">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <?php foreach($rows as $r): ?>
+            <div class="col-lg-3">
+                <div class="card">
+                    <img src="imgs/small/<?= $r['book_id'] ?>.jpg" class="card-img-top" alt="">
+                    <!-- 抓到圖片 -->
+                    
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $r['bookname'] ?></h5> 
+                        <p class="card-text"><?= $r['author'] ?></p>
+                        <p class="card-text"><?= $r['price'] ?></p>
+                        <!--複製bootstrap格式來修改  $r [' 資料庫欄位名稱 '] -->
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+</div>
 
 </div>
 <?php include __DIR__. '/0714_scripts.php' ?>
