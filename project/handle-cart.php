c6dbcf91e464e016c6eb6a7a27738f8c43bc86c1
esther-sing <s209261009@gmail.com>
2020-07-22 10:22:52 +0800

    handle-cart 2, splice

--- project/handle-cart.php
+++ project/handle-cart.php
@@ -8,7 +8,10 @@ if(! isset($_SESSION['cart'])){
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 $sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;  //編號
 $quantity = isset($_GET['quantity']) ? intval($_GET['quantity']) : 0;  //數量
-
+$output = [
+        'action' => $action, //動作
+        'code' => 0, //邏輯
+    ];
 /*
  * 加入商品, 移除商品, 查詢內容
  * action: add, remove, empty
@@ -31,6 +34,7 @@ switch ($action){
 
 }
 
-echo json_encode($_SESSION['cart'], JSON_UNESCAPED_UNICODE);
+$output['cart'] = $_SESSION['cart']; //在傳陣列裡加上陣列
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
 
 ?>
